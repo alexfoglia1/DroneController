2ea64fe176fe4b0744a1f0e7b2c74a57f0b22737
Alex Foglia <alex.f1793@gmail.com>
2022-04-16 04:02:49 +0200

    Handling radio timeout

--- firmware/DroneFirmware/FlightController/FlightController.ino
+++ firmware/DroneFirmware/FlightController/FlightController.ino
@@ -13,6 +13,9 @@
 #define MOTOR_PIN3 6
 #define MOTOR_PIN4 5
 
+#define RADIO_RX_FREQ 100
+#define MAX_PKT_LOSS  10
+
 #define APP_NAME "Arduino Flight Controller"
 #define MAJOR_VERSION '0'
 #define MINOR_VERSION '1'
@@ -28,12 +31,15 @@ RF24 radio(9,10);
 
 const uint64_t rx_pipe = 0xE6E6E6E6E6E6;
 const uint64_t tx_pipe = 0x6E6E6E6E6E6E;
+const uint64_t radioRxPeriod_micros = 1e6 / RADIO_RX_FREQ;
+uint64_t countToRxTimeout = 0;
 
 void setup(void)
 {
   Serial.begin(9600);
   printFullName();
-  
+  clearCommandMsg();
+    
   motor1.attach(MOTOR_PIN1);
   motor2.attach(MOTOR_PIN2);
   motor3.attach(MOTOR_PIN3);
@@ -42,21 +48,42 @@ void setup(void)
   radio.begin();
   radio.openReadingPipe(1, rx_pipe);
   radio.startListening();
+ 
 }
 
 void loop(void)
 {
-  clearCommandMsg();
-  
+  boolean radioRx = false;
   while (radio.available())
   {
     radio.read((char*)&commandMsg, sizeof(RadioToDroneCommandMessage));
+    radioRx = true;
+  }
+
+  int deltaTimeout;
+  if (!radioRx)
+  {
+    deltaTimeout = 1;
+  }
+  else
+  {
+    deltaTimeout = 0;
+  }
+
+  if (countToRxTimeout > MAX_PKT_LOSS * radioRxPeriod_micros)
+  {
+    countToRxTimeout = 0;
+    clearCommandMsg();
   }
   
   int DELAY = 0;
   if (RADIO_TO_DRONE_MSG_ID == commandMsg.msg_id)
   {
-   DELAY = jsAxisToSpeed(commandMsg.r2_axis); 
+    if (radioRx)
+    {
+      countToRxTimeout = 0;
+    }
+    DELAY = jsAxisToSpeed(commandMsg.r2_axis);
   }
   else
   {
@@ -70,7 +97,8 @@ void loop(void)
    
   float SPEED = (DELAY - 1000) / 10;
   Serial.print("\n");
-  Serial.println("Motor speed:"); Serial.print("  "); Serial.print(SPEED); Serial.print("%"); 
+  Serial.println("Motor speed:"); Serial.print("  "); Serial.print(SPEED); Serial.print("%");
+  countToRxTimeout += (4 * DELAY) * deltaTimeout;
 }
 
 void printFullName()
@@ -93,6 +121,7 @@ int jsAxisToSpeed(uint8_t jsAxis)
 
 void clearCommandMsg()
 {
+  Serial.println("Clearing command msg");
   commandMsg.msg_id = 0;
   commandMsg.r2_axis = 0;
   commandMsg.l2_axis = 0;
